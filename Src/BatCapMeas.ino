5735682f77a77cb01fcb1ae727efc54597de78ce
Jean-Paul PETILLON <Art-ut-Kia@users.noreply.github.com>
2020-02-17 22:00:37 +0100

    enriched comments
    and some cosmetic addings

--- Src/BatCapMeas.ino
+++ Src/BatCapMeas.ino
@@ -1,26 +1,28 @@
 /**
- * BatCapMeas.ino: Permits to measure the capacity, in [A x h], of a LiPo battery
+ * BatCapMeas.ino: Measures the capacity, in [A x h], of a Li-ion battery
+ * author: Jean-Paul PETILLON
+ * to be improved: average some samples to reduce noise
  */
 
 #define ledPin  13 // LED (shows current mode)
-#define gndPin  A0 // forced to OUTPUT, LOW to act as ground
-#define ctrlPin A1 // digital output that control the battery load (HIGH = load activated)
+#define gndPin  A0 // forced to OUTPUT, LOW to act as a ground pin
+#define ctrlPin A1 // digital output that controls the battery load (HIGH = load activated)
 #define measPin A2 // analog input that measures the battery voltage
 
-#define periodMinutes 5  // one sample each 5 minute => 42 hours autonomy (1500mAh with 100 Ohms load)
+#define periodMinutes 5  // one sample each 5 minutes
 
-int sample[500];
+int sample[500]; // buffer to memorize samples (not used so long)
 int i = 0;
 bool discharge = true;
 
 void setup() {
     Serial.begin(9600);
-    pinMode(ledPin,  OUTPUT); digitalWrite(ledPin,  LOW);
+    pinMode(ledPin,  OUTPUT); digitalWrite(ledPin,  LOW); // to witness the load status
     pinMode(gndPin,  OUTPUT); digitalWrite(gndPin,  LOW); // ground
     pinMode(ctrlPin, OUTPUT); digitalWrite(ctrlPin, LOW); // battery loading control
 
     bool start = false;
-    Serial.print("hit 'G' key to start discharge sequence: ");
+    Serial.print("hit 'g' key to start the discharge process: ");
     while (!start) {
         while (!Serial.available());
         start = (Serial.read()=='g');
@@ -42,6 +44,5 @@ void loop() {
     }
     Serial.print(discharge ? "discharging: " : "no load:     ");
     Serial.println(v);
-//  delay(periodMinutes * 60000);
-    delay(1000);
+    delay(periodMinutes * 60000);
 }
